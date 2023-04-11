d08f3c9477e308a58a9e377bca03160075263f38
ajz34 <ajz34@outlook.com>
2023-04-11 20:18:35 +0800

    refactor(dh eng): rmp2 refactor

--- pyscf/dh/__init__.py
+++ pyscf/dh/__init__.py
@@ -2,6 +2,6 @@ from . import util
 from . import energy
 
 from .energy import RDH, UDH, RDHBase, UDHBase
-from .energy.mp2 import RMP2ofDH, UMP2ofDH
+from .energy.mp2 import UMP2ofDH
 from .energy.iepa import RIEPAofDH, UIEPAofDH
 from .energy.ring_ccd import RRingCCDofDH
--- pyscf/dh/energy/mp2/__init__.py
+++ pyscf/dh/energy/mp2/__init__.py
@@ -1,3 +1,3 @@
 from . import rmp2, ump2
-from .rmp2 import RMP2ofDH
 from .ump2 import UMP2ofDH
+from .rmp2 import RMP2ConvPySCF, RMP2Conv, RMP2RI
