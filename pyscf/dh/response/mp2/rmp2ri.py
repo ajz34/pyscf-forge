5b2265fe2d1857c1869a40ca848bb80ac8505fca
ajz34 <ajz34@outlook.com>
2023-05-09 08:41:10 +0800

    test(dh resp): add test for rmp2ri dipole

--- pyscf/dh/response/mp2/rmp2ri.py
+++ pyscf/dh/response/mp2/rmp2ri.py
@@ -588,7 +588,7 @@ if __name__ == '__main__':
     def main_2():
         # test numerical dipole
         from pyscf import gto, scf, mp, df
-        np.set_printoptions(5, suppress=True, linewidth=150)
+        np.set_printoptions(8, suppress=True, linewidth=150)
         mol = gto.Mole(atom="O; H 1 0.94; H 1 0.94 2 104.5", basis="6-31G").build()
 
         def eng_with_dipole_field(t, h):
@@ -618,7 +618,36 @@ if __name__ == '__main__':
         print(dip_num)
         print(dip_anal)
 
-    main_2()
+    def main_3():
+        # test numerical dipole for DFT PT2
+        from pyscf import gto, scf, dft
+        np.set_printoptions(8, suppress=True, linewidth=150)
+        mol = gto.Mole(atom="O; H 1 0.94; H 1 0.94 2 104.5", basis="6-31G").build()
 
+        c_os, c_ss = 1.3, 0.6
 
-    main_1()
+        def eng_with_dipole_field(t, h):
+            mf_scf = dft.RKS(mol, xc="B3LYPg").density_fit("cc-pVDZ-jkfit")
+            mf_scf.get_hcore = lambda *args, **kwargs: scf.hf.get_hcore(mol) - h * mol.intor("int1e_r")[t]
+            mf_scf.run(conv_tol=1e-12)
+            mf_mp = RMP2RI(mf_scf).run()
+            return mf_mp.scf.e_tot + c_os * mf_mp.results["eng_corr_MP2_OS"] + c_ss * mf_mp.results["eng_corr_MP2_SS"]
+
+        mf_scf = dft.RKS(mol, xc="B3LYPg").density_fit("cc-pVDZ-jkfit").run()
+        mf_mp = RMP2RespRI(mf_scf).run(c_os=c_os, c_ss=c_ss)
+        dip_anal = mf_mp.make_dipole()
+
+        eng_array = np.zeros((2, 3))
+        h = 1e-4
+        for idx, h in [(0, h), [1, -h]]:
+            for t in (0, 1, 2):
+                eng_array[idx, t] = eng_with_dipole_field(t, h)
+        dip_elec_num = - (eng_array[0] - eng_array[1]) / (2 * h)
+        dip_nuc = np.einsum("A, At -> t", mol.atom_charges(), mol.atom_coords())
+        dip_num = dip_elec_num + dip_nuc
+
+        print(mf_scf.dip_moment(unit="AU"))
+        print(dip_num)
+        print(dip_anal)
+
+    main_3()
---
+++ pyscf/dh/response/mp2/test/test_rmp2ri.py
@@ -0,0 +1,62 @@
+import unittest
+from pyscf.dh.response.mp2.rmp2ri import RMP2RespRI
+from pyscf.dh.energy.mp2.rmp2 import RMP2RI
+import numpy as np
+
+
+class TestRMP2RI(unittest.TestCase):
+    def test_num_dipole_ref_hf(self):
+        from pyscf import gto, scf
+        mol = gto.Mole(atom="O; H 1 0.94; H 1 0.94 2 104.5", basis="6-31G").build()
+
+        def eng_with_dipole_field(t, h):
+            mf_scf = scf.RHF(mol).density_fit("cc-pVDZ-jkfit")
+            mf_scf.get_hcore = lambda *args, **kwargs: scf.hf.get_hcore(mol) - h * mol.intor("int1e_r")[t]
+            mf_scf.run(conv_tol=1e-12)
+            mf_mp = RMP2RI(mf_scf).run()
+            return mf_mp.e_tot
+
+        mf_scf = scf.RHF(mol).density_fit("cc-pVDZ-jkfit").run()
+        mf_mp = RMP2RespRI(mf_scf).run()
+        dip_anal = mf_mp.make_dipole()
+
+        eng_array = np.zeros((2, 3))
+        h = 1e-4
+        for idx, h in [(0, h), [1, -h]]:
+            for t in (0, 1, 2):
+                eng_array[idx, t] = eng_with_dipole_field(t, h)
+        dip_elec_num = - (eng_array[0] - eng_array[1]) / (2 * h)
+        dip_nuc = np.einsum("A, At -> t", mol.atom_charges(), mol.atom_coords())
+        dip_num = dip_elec_num + dip_nuc
+
+        self.assertTrue(np.allclose(dip_num, dip_anal, atol=1e-5, rtol=1e-7))
+
+    def test_num_dipole_ref_b3lyp(self):
+        from pyscf import gto, scf, dft
+        mol = gto.Mole(atom="O; H 1 0.94; H 1 0.94 2 104.5", basis="6-31G").build()
+
+        c_os, c_ss = 1.3, 0.6
+
+        def eng_with_dipole_field(t, h):
+            mf_scf = dft.RKS(mol, xc="B3LYPg").density_fit("cc-pVDZ-jkfit")
+            mf_scf.get_hcore = lambda *args, **kwargs: scf.hf.get_hcore(mol) - h * mol.intor("int1e_r")[t]
+            mf_scf.run(conv_tol=1e-12)
+            mf_mp = RMP2RI(mf_scf).run()
+            return mf_mp.scf.e_tot + c_os * mf_mp.results["eng_corr_MP2_OS"] + c_ss * mf_mp.results["eng_corr_MP2_SS"]
+
+        mf_scf = dft.RKS(mol, xc="B3LYPg").density_fit("cc-pVDZ-jkfit").run()
+        mf_mp = RMP2RespRI(mf_scf).run(c_os=c_os, c_ss=c_ss)
+        dip_anal = mf_mp.make_dipole()
+
+        eng_array = np.zeros((2, 3))
+        h = 1e-4
+        for idx, h in [(0, h), [1, -h]]:
+            for t in (0, 1, 2):
+                eng_array[idx, t] = eng_with_dipole_field(t, h)
+        dip_elec_num = - (eng_array[0] - eng_array[1]) / (2 * h)
+        dip_nuc = np.einsum("A, At -> t", mol.atom_charges(), mol.atom_coords())
+        dip_num = dip_elec_num + dip_nuc
+
+        self.assertTrue(np.allclose(dip_num, dip_anal, atol=1e-5, rtol=1e-7))
+
+
