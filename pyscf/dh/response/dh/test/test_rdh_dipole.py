ab6a350f2dd6ac3522b6bd09dbcf85cb3e0086a1
ajz34 <ajz34@outlook.com>
2023-06-15 20:26:10 +0800

    test(dh dipole): add some tests for wB97X-2 (unrestricted dipole not included ...)

--- pyscf/dh/response/dh/test/test_rdh_dipole.py
+++ pyscf/dh/response/dh/test/test_rdh_dipole.py
@@ -87,3 +87,35 @@ class TestRDHResp(unittest.TestCase):
         dip_num = dip_elec_num + dip_nuc
         self.assertTrue(np.allclose(dip_anal, dip_num, rtol=1e-5, atol=1e-7))
         """
+
+    def test_response_den_wB97X_2_TQZ(self):
+        np.set_printoptions(8, suppress=True, linewidth=150)
+        mol = gto.Mole(atom="O; H 1 0.94; H 1 0.94 2 104.5", basis="6-31G").build()
+        xc_code = "wB97X-2-TQZ"
+        mf_resp = RDHResp(mol, xc=xc_code).run()
+        dip_anal = mf_resp.make_dipole()
+
+        REF = np.array([0.60742951, 0., 0.78449958])
+        self.assertTrue(np.allclose(dip_anal, REF, atol=1e-5, rtol=1e-7))
+
+        """
+        # generation of numerical result
+
+        def eng_with_dipole_field(t, h):
+            mf_mp = RDH(mol, xc=xc_code)
+            mf_mp.scf.get_hcore = lambda *args, **kwargs: scf.hf.get_hcore(mol) - h * mol.intor("int1e_r")[t]
+            mf_mp.scf.conv_tol = 1e-12
+            mf_mp.run()
+            return mf_mp.e_tot
+
+        eng_array = np.zeros((2, 3))
+        h = 1e-4
+        for idx, h in [(0, h), [1, -h]]:
+            for t in (0, 1, 2):
+                eng_array[idx, t] = eng_with_dipole_field(t, h)
+        dip_elec_num = - (eng_array[0] - eng_array[1]) / (2 * h)
+
+        dip_nuc = np.einsum("A, At -> t", mol.atom_charges(), mol.atom_coords())
+        dip_num = dip_elec_num + dip_nuc
+        self.assertTrue(np.allclose(dip_anal, dip_num, rtol=1e-5, atol=1e-7))
+        """
